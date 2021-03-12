23358b1c80b298d32d4265d969760cfc2b3c5a4d
usrbinkat <usrbinkat@braincraft.io>
2021-03-12 23:39:28 +0000

    working download and image build

--- cradles/rhcos/script.py
+++ cradles/rhcos/script.py
@@ -7,45 +7,88 @@ import tempfile
 import argparse
 import subprocess
 import urllib.request
-
-from io import StringIO
-from io import BytesIO
-from pathlib import Path
-from tempfile import NamedTemporaryFile
-
-run_path = os.path.realpath(__file__)
-rhcos_archive_openstack = "rhcos-openstack.x86_64.qcow2.gz"
-rhcos_download_url = "http://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/latest"
-podman_build_args = "podman build -t rhcos:4.7"
+#from io import StringIO
+#from io import BytesIO
+#from pathlib import Path
+#from tempfile import NamedTemporaryFile
 
 
-def main():
-    DownloadRHCOS(rhcos_download_url, rhcos_archive_openstack)
-    PodmanBuildCradle(podman_build_args, run_path)
+# Define Mandatory & Optional CLI Flags
+parser = argparse.ArgumentParser(
+    description='KubeVirt Image Cradle Builder to generate OpenShift Virtualization compatible qcow2 import images')
+parser.add_argument(
+    "--image-name",
+    default="localhost/cradle",
+    help="Name of cradle image",
+    metavar="registry.com/namespace/image-name",
+    required=True)
+parser.add_argument(
+    "--image-tag",
+    default="latest",
+    help="Image tag",
+    metavar="IMAGE_TAG",
+    required=False)
+parser.add_argument(
+    "--build-path",
+    default=(os.getcwd()),
+    help="Run directory for script, must be an absolute path, only handy if running script in a container",
+    required=False)
 
 
-def PodmanBuildCradle(podman_build_args, run_path):
-    print( ">> Building Cradle from: " + run_path )
-    subprocess.run(podman_build_args + run_path, shell=True, check=True)
+group = parser.add_mutually_exclusive_group(
+    required=True)
+group.add_argument(
+    "--image-url",
+    default="http://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/latest/rhcos-openstack.x86_64.qcow2.gz",
+    metavar="https://FQDN/URI/image.qcow2.gz",
+    help="qcow image url")
+group.add_argument(
+    "--image-file",
+    metavar="/global/path/to/image.qcow2",
+    help="qcow image path")
 
 
-def DownloadRHCOS(rhcos_download_url, rhcos_archive_openstack):
-    download_url = urllib.request.urlopen(rhcos_download_url + rhcos_archive_openstack)
-    rhcos_image = rhcos_archive_openstack[:-3]
-    print("Downloading RHCOS from: " + download_url)
+# Parse cli arguments
+args                     = parser.parse_args()
+
+
+# Base Variables
+cradle_tag               = args.image_tag
+cradle_name              = args.image_name
+build_path               = args.build_path
+container_build_cmd      = "podman build"
+qcow2_image_staging_path = ( build_path + "/rootfs/disk/image.qcow2" )
+
+
+# Download RHCOS Image from OpenShift mirror site
+def DownloadRHCOS(args, build_path):
+    download_url = (args.image_url)
+    print( ">> Downloading RHCOS Image: " + args.image_url )
     try:
-        with urlopen(download_url) as archive_file:
-            with gzip.GzipFile(fileobj=archive_file) as archive:
-                archive_content = uncompressed.read()
-
-            with open(out_file, 'wb') as image:
+        with urllib.request.urlopen(download_url) as image_gzip:
+            with gzip.GzipFile(fileobj=image_gzip) as archive:
+                archive_content = archive.read()
+            with open(qcow2_image_staging_path, 'wb') as image:
                 image.write(archive_content)
                 return 0
-        
     except Exception as e:
         print(e)
         return 1
 
+
+# Build Disk Image Cradle Container
+def PodmanBuildCradle(container_build_cmd, cradle_name, cradle_tag, build_path):
+    subprocess.run(
+        container_build_cmd + " -t " + cradle_name + ":" + cradle_tag + " " + build_path,
+        shell=True, check=True
+    )
+
+
+def main():
+    print( ">> Automated KubeVirt Qcow2 Image Cradle Container Builder" )
+    print( ">> Building Cradle from directory: " + build_path )
+    DownloadRHCOS(args, build_path)
+    PodmanBuildCradle(container_build_cmd, cradle_name, cradle_tag, build_path)
 
 
 if __name__ == "__main__":
