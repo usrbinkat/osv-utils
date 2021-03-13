003502525c8a7c071e2a439a9cb9f28025385c7a
usrbinkat <usrbinkat@braincraft.io>
2021-03-13 00:03:02 +0000

    rough draft
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- cradles/rhcos/script.py
+++ cradles/rhcos/script.py
@@ -39,12 +39,16 @@ group = parser.add_mutually_exclusive_group(
     required=True)
 group.add_argument(
     "--image-url",
+    help="qcow image url",
     metavar="https://FQDN/URI/image.qcow2.gz",
-    help="qcow image url")
+    default="",
+    required=False)
 group.add_argument(
     "--image-file",
+    help="qcow image path",
     metavar="/global/path/to/image.qcow2",
-    help="qcow image path")
+    default="",
+    required=False)
 
 
 # Parse cli arguments
@@ -59,10 +63,14 @@ container_build_cmd      = "podman build"
 qcow2_image_staging_path = ( build_path + "/rootfs/disk/image.qcow2" )
 
 
-# Download RHCOS Image from OpenShift mirror site
-def DownloadRHCOS(args, build_path):
+# Stage Qcow2 Image File
+def StageImageFile(args, build_path):
+    print( ">> Staging Image: " + args.image_file )
+
+# Download Image from URL
+def DownloadImageFile(args, build_path):
     download_url = (args.image_url)
-    print( ">> Downloading RHCOS Image: " + args.image_url )
+    print( ">> Downloading Image: " + args.image_url )
     try:
         with urllib.request.urlopen(download_url) as image_gzip:
             with gzip.GzipFile(fileobj=image_gzip) as archive:
@@ -86,7 +94,10 @@ def PodmanBuildCradle(container_build_cmd, cradle_name, cradle_tag, build_path):
 def main():
     print( ">> Automated KubeVirt Qcow2 Image Cradle Container Builder" )
     print( ">> Building Cradle from directory: " + build_path )
-    DownloadRHCOS(args, build_path)
+    if args.image_url != "":
+      DownloadImageFile(args, build_path)
+    if args.image_file != "":
+      StageImageFile(args, build_path)
     PodmanBuildCradle(container_build_cmd, cradle_name, cradle_tag, build_path)
 
 
